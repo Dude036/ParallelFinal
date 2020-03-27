52937c902bc6ae31f16469462c355f1652f902e6
Joshua Higham <josh@higham.com>
2020-03-27 11:56:02 -0600

    More setup

--- main.cpp
+++ main.cpp
@@ -1,57 +1,72 @@
 #include <iostream>			// cout
 #include <algorithm>		// qsort, swap
 #include <random>			// random_device, mt19937, shuffle
-#include <string>			// striog
+#include <string>			// string
 #include <vector>			// Vector
 #include <mpi.h>			// MPI everything
 
 #define MCW MPI_COMM_WORLD
 #define VERBOSE true
 #define MASTER 0
+/*
+Reference: https://www.youtube.com/watch?v=s6FhG--P7z0
+*/
 
-
+/* Main function */
 int main(int argc, char *argv[]) {
 	// MPI Setup
-	int rank, size;
+	int mpi_rank, mpi_size;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MCW, &rank);
-	MPI_Comm_size(MCW, &size);
+	MPI_Comm_rank(MCW, &mpi_rank);
+	MPI_Comm_size(MCW, &mpi_size);
 
 	// Set and sum size
 	int* set;
-	int set_size = 100, set_sum = 100;
+	int job_size = 10;
+	int set_size = mpi_size * job_size;
+	int set_sum = set_size * job_size;
+
+	// Allocate new array
 	set = new int[set_size];
 
 	// Construct data set to be broadcast
-	if (rank == MASTER) {
+	if (mpi_rank == MASTER) {
 		// Random initializer
 		std::random_device rd;
 		std::mt19937 g(rd());
 
-
 		// Allocate numbers between [1, set_sum)
 		for (int i = 0; i < set_size; ++i) {
-			set[i] = g() % (set_sum - 1) + 1;
+			set[i] = g() % (set_sum / 2 - 1) + 1;
 		}
 	}
 
 	// MPI shares info about the data
 	MPI_Bcast(set, set_size, MPI_INT, MASTER, MCW);
 
-	if (VERBOSE) {
-		std::string output = "[" + std::to_string(rank) + "] = {";
-		for (int i = 0; i < set_sum; ++i) {
+	if (VERBOSE && mpi_rank == MASTER) {
+		std::string output = "{";
+		for (int i = 0; i < set_size; ++i) {
 			output += std::to_string(set[i]) + ",";
 		}
 		std::cout << output << "}" << std::endl;
+		std::cout << "Attempting to fill a subset with size " << set_sum << std::endl;
 	}
 
 	// Allocate for subset
 	bool** subset_grid = new bool*[set_size];
 	for (int i = 0; i < set_size; ++i) {
 		subset_grid[i] = new bool[set_sum + 1];
+		// setup before dynamic programming
+		subset_grid[i][0] = true;
 	}
 
+	// Start dynamic programming process
+	for (int i = 0; i < set_size; ++i) {
+		// determine previous solution set
+
+		// 
+	}
 
 	// Deallocate variable pointers
 	for (int i = 0; i < set_size; ++i) {
